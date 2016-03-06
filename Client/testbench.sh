7720fbb8d8ddc79f3710327080dd748dcb6c4e06
Honglu <yakyakhe@gmail.com>
2016-03-06 10:19:44 +0800

    Change number of clients to a argument of the testbench

--- Client/testbench.sh
+++ Client/testbench.sh
@@ -1,14 +1,17 @@
 
 #!/bin/bash
+num_of_clients=$1
+
 export PATH=$PATH:$(pwd)
+
 set -m # Enable Job Control
 
 #######  create "IRS" account
 client.sh -o 0
 
-#######  open 100 accounts first
-echo "Open 100 accounts"
-for i in `seq 100`; do # start 100 jobs in parallel
+#######  open $num_of_clients accounts first
+echo "Open $num_of_clients accounts"
+for i in `seq $num_of_clients`; do # start $num_of_clients jobs in parallel
   client.sh -o $i &
 done
 # Wait for all parallel jobs to finish
@@ -17,7 +20,7 @@ while [ 1 ]; do fg 2> /dev/null; [ $? == 1 ] && break; done
 
 ######  deposit 200 to each account
 echo "Deposit 200 into each account"
-for i in `seq 100`; do # start 30 jobs in parallel
+for i in `seq $num_of_clients`; do 
   client.sh -d $i 200 &
 done
 # Wait for all parallel jobs to finish
@@ -25,7 +28,7 @@ while [ 1 ]; do fg 2> /dev/null; [ $? == 1 ] && break; done
 
 ######  withdraw 100 from each account and transfer 1 to IRS
 echo "Withdraw 100 from each account, and transfer 1 to account 0"
-for i in `seq 100`; do # start 30 jobs in parallel
+for i in `seq $num_of_clients`; do 
   client.sh -w $i 100 &
   client.sh -t $i 0 1 &
 done
@@ -33,7 +36,7 @@ done
 while [ 1 ]; do fg 2> /dev/null; [ $? == 1 ] && break; done
 
 ########### Now check the IRS account, it should have $100
-echo "Now check the IRS account, it should have $ 100"
+echo "Now check the IRS account, it should have $num_of_clients"
 client.sh -q 0
 sleep 5
 
